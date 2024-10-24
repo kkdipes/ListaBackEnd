9d1c4b8290d106ad72d755d951471c6afe2a93a4
kkdipes <165501393+kkdipes@users.noreply.github.com>
2024-10-24 09:37:59 -0300

    Add files via upload

---
+++ atividade0/src/main/java/com/lista/exercicios/ExerciciosbackendApplication.java
@@ -0,0 +1,13 @@
+package com.lista.exercicios;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ExerciciosbackendApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ExerciciosbackendApplication.class, args);
+	}
+
+}
